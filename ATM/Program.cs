93716e1da9b8f7aa3e5ca2d7d525935a37eae152
Admin <Admin@DESKTOP-7KR487T>
2022-09-12 10:39:24 +0100

    Add project files.

---
+++ ATM/Program.cs
@@ -0,0 +1,171 @@
+﻿namespace ATM
+
+{
+    public class cardHolder
+    {
+        string cardNum;
+        int pin;
+        string firstName;
+        string lastName;
+        double balance;
+        public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+        {
+            this.cardNum = cardNum;
+            this.pin = pin;
+            this.firstName = firstName; 
+            this.lastName = lastName;
+            this.balance = balance;
+        }
+        public string getNum() 
+        {
+            return cardNum;
+        }
+        public int getPin()
+        {
+            return pin;
+        }
+        public string getFirstName()
+        {
+            return firstName;
+        }
+        public string getlastName()
+        {
+            return lastName;
+        }
+        public double getBalance()
+        {
+            return balance;
+        }
+        public void setNum(string newCardNum)
+        {
+            cardNum = newCardNum;
+        }
+        public void setpin(int newPin)
+        {
+            pin = newPin;
+        }
+        public void setFirstName(string newFirstName)
+        {
+            firstName = newFirstName;
+        }
+        public void setLastName(string newLastName)
+        {
+            lastName = newLastName;
+        }
+        public void setBalance(double newBalance)
+        {
+            balance = newBalance;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.Title = "ATM Machine";
+            Console.ForegroundColor = ConsoleColor.White;
+            void printOptions()
+            {
+                Console.WriteLine("Choose from the following menu...");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Show Balance");
+                Console.WriteLine("4. Exit");
+            }
+            void deposit(cardHolder currentUser)
+            {
+                Console.WriteLine("How much $$$ do you want to deposit: ");
+
+                double deposit = Double.Parse(Console.ReadLine());
+                currentUser.setBalance(deposit + currentUser.getBalance());
+
+                Console.WriteLine("Thank you for your $$$, your new balance is: {0:C}", currentUser.getBalance());
+            }
+            void withdraw(cardHolder currentUser)
+            {
+                Console.WriteLine("How much $$$ do you want to withdraw: ");
+                double withdrawal = Double.Parse(Console.ReadLine());
+                //Check if the user balance is enough
+                if(withdrawal > currentUser.getBalance())
+                {
+                    Console.WriteLine("Insufficient Balance!!!");
+                }
+                else
+                {
+                    currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                    Console.WriteLine("You are good to go, thank you!");
+                }
+
+            }
+            void balance(cardHolder currentUser)
+            {
+                Console.WriteLine("Your current balance is: {0:C}", currentUser.getBalance());
+            }
+
+            List<cardHolder> cardHolders = new List<cardHolder>()
+            {
+                new cardHolder("5197149864619736", 6189, "Mildred", "Barnhardt", 525.34),
+                new cardHolder("4052617275803096", 2561, "Eddie", "Edwards", 986.4),
+                new cardHolder("5192142996284532", 1234, "Wade", "Abney", 466.787),
+                new cardHolder("4099215499130126", 7385, "Allen", "Rivera", 235.26),
+                new cardHolder("4074588175011474", 9124, "Casey", "Donovan", 353.6),
+                new cardHolder("377384157170963", 6242,  "Laverne", "Martin" ,532.53),
+                new cardHolder("4567019349086188", 1436, "Cecelia", "Bucholz", 55.6)
+            };
+
+
+            Console.WriteLine("Welcome to simple ATM");
+
+            Console.WriteLine("Please insert yout debit card: ");
+            string debitCardNum = "";
+            cardHolder currentUser;
+
+            while (true)
+            {
+                try
+                {
+                debitCardNum = Console.ReadLine();  
+                //check against our database
+                currentUser = cardHolders.FirstOrDefault(p => p.cardNum == debitCardNum);
+                if(currentUser != null) { break; }
+                else { Console.WriteLine("Card not recognized!, please try again"); }
+                }
+                catch { Console.WriteLine("Card not recognized!, please try again"); }
+            }
+
+            Console.WriteLine("Please insert your pin: ");
+            int userPin = 0;
+            while (true)
+            {
+                try
+                {
+                    userPin = int.Parse(Console.ReadLine());
+                    //check against our db
+                    if (currentUser.getPin() == userPin) { break; }
+                    else { Console.WriteLine("Incorrect Pin!, please try again"); }
+                }
+                catch { Console.WriteLine("Incorrect Pin!, please try again"); }
+            }
+
+            Console.WriteLine("Welcome " + currentUser.getFirstName());
+            int option = 0;
+
+            do
+            {
+                printOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch { }
+               if(option == 1) { deposit(currentUser); }
+               else if (option == 2) { withdraw(currentUser); }
+               else if (option == 3) { balance(currentUser); }
+               else if (option == 4) { break; }
+                else { option = 0; }
+            } while (option != 4);
+            Console.WriteLine("Thank you, have a nice day.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyATMMachine/Program.cs
@@ -0,0 +1,206 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+class cardHolder
+{
+    public string cardNum { get; set; }
+    public int pin { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public double balance { get; set; }
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    string requestPIN()
+    {
+        StringBuilder sb = new StringBuilder();
+        ConsoleKeyInfo keyInfo;
+
+        int i = 0;
+        do
+        {
+            keyInfo = Console.ReadKey(true);
+            
+            if(!char.IsControl(keyInfo.KeyChar))
+            {
+                sb.Append(keyInfo.KeyChar);
+                Console.Write("*");
+                i++;
+                
+            }
+        }while(keyInfo.Key != ConsoleKey.Enter);
+        Console.WriteLine("\n");
+        {
+                return sb.ToString();
+        }
+    }
+
+    int printOptions()
+    {
+        int choice;
+        do
+        {
+            choice = 0;
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Choose an option: \n1. Deposit\n2. Withdraw\n3. Show balance\n4. Exit");
+                Console.ForegroundColor = ConsoleColor.Green;
+                choice = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+                if (choice == 1 || choice == 2 || choice == 3 || choice == 4) { break; }
+                else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Choise. choose a number from 1 to 4."); }
+            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Choise. choose a number from 1 to 4."); }
+
+        } while (true);
+        return choice;
+    }
+    static cardHolder cardNumVer(List<cardHolder> cardHolders)
+    {
+        cardHolder currentUser;
+        string cardEntered = "";
+        do
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter your card number: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                cardEntered = Console.ReadLine();
+                currentUser = cardHolders.FirstOrDefault(p => p.cardNum == cardEntered);
+                if (currentUser != null) { break; }
+                else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Card Number!, try again."); }
+            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Card Number!, try again."); }
+        } while (true);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Card Number Verified.");
+        return currentUser;
+    }
+    void pinVer(cardHolder currentUser)
+    {
+        int pin = 0;
+        do
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter your Pin number: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                pin = int.Parse(requestPIN());
+                if (pin == currentUser.pin) { break; }
+                else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Pin!, try again."); }
+            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Pin!, try again."); }
+        } while (true);
+    }
+    void deposit(cardHolder currentUser)
+    {
+        double moneyEntered;
+        
+        do
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("How much do you want to deposit?");
+                Console.ForegroundColor = ConsoleColor.Green;
+                moneyEntered = double.Parse(Console.ReadLine());
+                if (moneyEntered > 0) 
+                { 
+                    currentUser.balance += moneyEntered;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("The operation succeded!,thanks a lot"); 
+                    break; 
+                }
+                else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Try again, Invalid input!"); }
+            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Try again, Invalid input!"); }
+
+        } while (true);
+
+    }
+    void withdraw(cardHolder currentUser)
+    {
+        double withdrawal = 0;
+        do
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("How much you want to withdraw :");
+                Console.ForegroundColor = ConsoleColor.Green;
+                withdrawal = double.Parse(Console.ReadLine());
+                if (withdrawal < currentUser.balance) 
+                { 
+                    currentUser.balance -= Math.Abs(withdrawal);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Operation succeded!, Thanks a lot."); 
+                    break;
+                }
+                else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Your money is not enough!, please try again."); }
+            }
+            catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid Input!, please try again."); }
+        } while (true);
+
+    }
+    void showBalance(cardHolder currentUser)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Your balance is: {0:C}", currentUser.balance);
+    }
+    private static int Main(string[] args)
+    {
+        List<cardHolder> cardHolders = new List<cardHolder>()
+        {
+                new cardHolder("5197149864619736", 6189, "Mildred", "Barnhardt", 525.34),
+                new cardHolder("4052617275803096", 2561, "Eddie", "Edwards", 986.4),
+                new cardHolder("5192142996284532", 1234, "Wade", "Abney", 466.787),
+                new cardHolder("4099215499130126", 7385, "Allen", "Rivera", 235.26),
+                new cardHolder("4074588175011474", 9124, "Casey", "Donovan", 353.6),
+                new cardHolder("377384157170963", 6242,  "Laverne", "Martin" ,532.53),
+                new cardHolder("4567019349086188", 1436, "Cecelia", "Bucholz", 55.6)
+        };
+        //Sets the title of the console
+        Console.Title = "    .......* ATM Machine *.......";
+        //Sets the text color of the console also known as the foreground color 
+        Console.ForegroundColor = ConsoleColor.White;
+
+            cardHolder currentUser = cardNumVer(cardHolders);
+            currentUser.pinVer(currentUser);
+
+            Console.WriteLine("Welcome {0}!", currentUser.firstName);
+        while (true)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            int userChoice = currentUser.printOptions();
+            if (userChoice == 4)
+            {
+                Console.WriteLine("Thank you for your time.");
+                Console.ReadKey();
+                return 0;
+            }
+            else if (userChoice == 1)
+            {
+                currentUser.deposit(currentUser);
+            }
+            else if (userChoice == 2)
+            {
+                currentUser.withdraw(currentUser);
+            }
+            else if (userChoice == 3)
+            {
+                currentUser.showBalance(currentUser);
+            }
+        Console.ReadKey();
+        }
+    }
+}
